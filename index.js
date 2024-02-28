adbd32c51fc95ab17417c90500a1aa3ba9345521
Oluwatomisin <kanmitomi@gmail.com>
2024-02-28 21:54:19 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-var i;
+var menu =  document.getElementById("menu")
+var closeMenu =  document.getElementById("closeMenu")
+var linkDiv =  document.getElementById("navLinks")
+
+closeMenu.addEventListener("click", () => {
+    linkDiv.style.display = "none"
+    menu.style.display = "flex"
+})
+
+menu.addEventListener("click", () => {
+    linkDiv.style.display = "flex"
+})
